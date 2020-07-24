e4890073197bd39290cd9f0a7268f9623213c39e
Luciano Junior <lucianojuniorarl@gmail.com>
2020-07-24 02:07:54 -0300

    config moeda R$

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,17 @@
 //teste
+const Mask = {
+    apply(input, func){
+        setTimeout(function(){
+            input.value = Mask[func](input.value)
+        },1)
+    },
+    formatBRL(value){
+        value = value.replace(/\D/g,"")
+
+      
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100)
+    }
+}
