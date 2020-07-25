6f47b66564c4158dc7e5e306dad63e83f181f4ed
Luciano Junior <lucianojuniorarl@gmail.com>
2020-07-24 22:36:18 -0300

    criando o edit, alterando o styles.css

---
+++ ProductController.js
@@ -0,0 +1,48 @@
+const Product = require('../models/Product')
+const Category = require('../models/Category')
+const { formatPrice } = require('../../lib/utils')
+
+module.exports = {
+    create(req,res) {
+        //pegar as categorias
+        Category.all()
+        .then(function(results) {
+            const categories = results.rows;
+            return res.render('products/create.njk', { categories })
+        }).catch(function(err){
+            throw new Error(err)
+        })
+    },
+
+    async post(req, res){
+        //logica de salvar
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == '') {
+                return res.send('Por favor, preencha o formulario')
+            }
+        }      
+        
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        return res.redirect(`/products/${productId}`)
+    },   
+    
+    async edit(req,res){
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product)return res.send('Product not found!!')
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
+
+        results = await Category.all()
+        const categories = results.rows
+
+        return res.render('products/edit.njk', { product, categories })
+    }
+}
