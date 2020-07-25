e972585ea486451d17f0cca2c65bd970dc23a401
Luciano Junior <lucianojuniorarl@gmail.com>
2020-07-25 00:05:11 -0300

    Editar e excluir produtos

--- ProductController.js
+++ ProductController.js
@@ -2,6 +2,8 @@ const Product = require('../models/Product')
 const Category = require('../models/Category')
 const { formatPrice } = require('../../lib/utils')
 
+
+
 module.exports = {
     create(req,res) {
         //pegar as categorias
@@ -20,7 +22,7 @@ module.exports = {
 
         for (key of keys) {
             if (req.body[key] == '') {
-                return res.send('Por favor, preencha o formulario')
+                return res.send('Por favor, preencha o formulario - ProductController - POST')
             }
         }      
         
@@ -34,7 +36,7 @@ module.exports = {
         let results = await Product.find(req.params.id)
         const product = results.rows[0]
 
-        if(!product)return res.send('Product not found!!')
+        if(!product)return res.send('Product not found - ProductController - Edit!!')
 
         product.old_price = formatPrice(product.old_price)
         product.price = formatPrice(product.price)
@@ -44,5 +46,33 @@ module.exports = {
         const categories = results.rows
 
         return res.render('products/edit.njk', { product, categories })
+    },
+
+    async put(req,res){
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == '') {
+                return res.send('Por favor, preencha o formulario - ProductController - PUT')
+            }
+        } 
+
+        req.body.price = req.body.price.replace(/\D/g, "")
+
+        if(req.body.price != req.body.old_price){
+            const oldProduct = await Product.find(req.body.id)
+
+            req.body.old_price = oldProduct.rows[0].price
+        }
+
+        await Product.update(req.body)
+
+        return res.redirect(`/products/${req.body.id}/edit`)
+    },
+
+    async delete(req, res){
+        await Product.delete(req.body.id)
+
+        return res.redirect('/products/create')
     }
 }
---
+++ routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const routes = express.Router()
+const ProductController = require('./app/controllers/ProductController')
+
+
+//busca recursos, informação
+routes.get('/',(req,res)=>{
+    return res.render("layout.njk")
+})
+
+routes.get('/products/create',ProductController.create)
+routes.get('/products/:id/edit',ProductController.edit)
+
+routes.post('/products',ProductController.post)
+routes.put('/products',ProductController.put)
+routes.delete('/products',ProductController.delete)
+
+
+//alias
+routes.get('/ads/create',(req,res)=>{
+    return res.redirect("/products/create")
+})
+
+
+module.exports = routes;
